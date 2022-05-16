eee210ecb3843d095326d3a41fc6ee39dd76b346
Romuald Caplier <caplier.r@gmail.com>
2022-05-16 23:03:54 +0200

    improvments

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
+import array
+import math
+import time
 from xmlrpc.client import Boolean
-from infi.systray import SysTrayIcon
+
 import keyboard
-import win32gui
-import win32process
 import win32api
 import win32clipboard
-import time
-import math
+import win32con
+import win32gui
+import win32process
+from infi.systray import SysTrayIcon
 
 hover_text = "JBVBA"
 vbaClientWhDl = -1
+projectWhDl = -1  # handle to the let project docker todo
+codeWhDl = -1
 
 quitApp = False
 
@@ -20,11 +25,29 @@ def bye(sysTrayIcon):
     print('Bye, then.')
 
 
-# Callback to get VBA windows handle
+def cbGetCodeWin(hwnd, param):
+    global codeWhDl
+    if param in win32gui.GetWindowText(hwnd):
+        codeWhDl = hwnd
+
+
+# Callback to get VBA windows handle AND code window handle (only work if code window is maximised for now)
 def cbGetVBAWin(hwnd, extra):
-    global vbaClientWhDl
+    global vbaClientWhDl, codeWhDl
     if "Microsoft Visual Basic" in win32gui.GetWindowText(hwnd):
         vbaClientWhDl = hwnd
+        main_window_title = win32gui.GetWindowText(hwnd)
+        if len(main_window_title.split("-")) == 3:  # if code window is maximised there is the name of
+            # the window appended to the main VBA window title (separated by a new "-")
+
+            # We have to clean it first (remove the brackets around)
+            code_window_title = str(main_window_title.split("-")[2]).strip()
+
+            if "[" in code_window_title and "]" in code_window_title:
+                code_window_title = code_window_title.replace("[", "", 1)
+                code_window_title = code_window_title.replace("]", "", 1)
+
+            win32gui.EnumChildWindows(vbaClientWhDl, cbGetCodeWin, code_window_title)
 
 
 # Method to check if VBA editor window is foreground ( todo : and has focus)
@@ -113,17 +136,81 @@ def commentLine():
         keyboard.send('end')
 
 
+def test4cb(hwnd, param):
+    print("child")
+    print(str(hwnd))
+
+
+def test3cb(hwnd, param):
+    print(win32gui.GetClassName(hwnd))
+    if win32gui.GetClassName(hwnd) == "ObtbarWndClass":
+        test = win32gui.EnumChildWindows(hwnd, test4cb, None)
+        print("test")
+        print(str(test))
+        # test = win32gui.GetDlgCtrlID(hwnd)
+        # print(test)
+
+        # test = win32gui.GetDlgItemText(hwnd, test)
+
+        # test = win32gui.SendMessage(test, win32con.CB_GETLBTEXTLEN, 0, 0)
+        # print(str(test))
+
+        cb_len = win32gui.SendMessage(hwnd, win32con.EM_GETSEL, 0, 0)
+        print("Text length : " + str(cb_len))
+        # buffer = win32gui.PyMakeBuffer(cb_len)
+        # test = win32gui.SendMessage(hwnd, win32con.CB_GETLBTEXT, 0, buffer)
+        # print(str(test))
+        # text = buffer
+        # print(text)
+
+        buffer = win32gui.PyMakeBuffer(cb_len)
+
+        win32gui.SendMessage(hwnd, win32con.WM_GETTEXTLENGTH, cb_len, buffer)  # read the text
+
+        address, length = win32gui.PyGetBufferAddressAndLen(buffer)
+
+        text = win32gui.PyGetString(address, length)
+
+        print(text)
+    return True
+
+
+def getProjectCB(hwnd, param):
+    global projectWhdl
+    if "Project - " in win32gui.GetWindowText(hwnd):
+        projectWhdl = hwnd
+
+
 def getSelection():
     test_var = ""
     if checkIfVBAIDEHasFocus():
+        resp = win32gui.SendMessage(codeWhDl, win32con.WM_COPY, None, None)
+        print(str(resp))
+
+        win32gui.EnumChildWindows(codeWhDl, test3cb, None)
+
+        # win32gui.GetDlgItem(projectWhdl)
+
+        # test_var2 = str(win32gui.GetFocus())
+        # print(vbaClientWhDl)
+        # print(test_var2)
+
+        # dc = win32gui.GetWindowDC(vbaClientWhDl)
+        # print(str(dc))
+
+        # keyboard.press('ctrl')
+        # keyboard.press('c')
+        # keyboard.release('c')
+        # keyboard.release('ctrl')
+
         win32clipboard.OpenClipboard(vbaClientWhDl)
-        win32clipboard.EmptyClipboard()
-        keyboard.press('ctrl')
-        keyboard.press('c')
-        #todo here
-        keyboard.release('ctrl')
-        keyboard.release('c')
-        print(win32clipboard.GetClipboardData(1))
+        # win32clipboard.EmptyClipboard()
+        # keyboard.press('ctrl')
+        # keyboard.press('c')
+
+        # keyboard.release('ctrl')
+        # keyboard.release('c')
+        # print(str(win32clipboard.GetClipboardData(win32con.CF_UNICODETEXT)))
         win32clipboard.CloseClipboard()
 
 
@@ -158,6 +245,7 @@ def callback(event):
 #                                               ))
 #                )
 # sysTrayIcon = SysTrayIcon("py.ico", hover_text, menu_options, on_quit=bye, default_menu_index=1)
+# TODO : Clique gauche = donne le focus à la window VBA
 sysTrayIcon = SysTrayIcon("ok.ico", hover_text, on_quit=bye, default_menu_index=1)
 sysTrayIcon.start()
 
